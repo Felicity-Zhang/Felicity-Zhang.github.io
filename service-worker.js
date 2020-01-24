4420514f1ae3effc4316b28c157d70d0b391ba94
Jiaxin-Pei <pedropei@vip.qq.com>
2020-01-24 14:30:55 -0500

    new#

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31a9b5d05a76958b6a73e72d35890310.js"
+  "/precache-manifest.8b85128d962d89aa082118441abf2c8f.js"
 );
 
 self.addEventListener('message', (event) => {
